26ef37f5c64893d9d5bed53453ffb3da95f916a5
Shepico <Shepico@gmail.com>
2018-07-28 16:17:36 +0400

    Подсчитана сумма выполнен возврат

--- src/lesson2/MainClass.java
+++ src/lesson2/MainClass.java
@@ -3,14 +3,15 @@ package lesson2;
 public class MainClass {
     public static void main (String[] args) {
         String str = "1 3 1 2\n2 3 2 2\n5 6 7 1\n3 3 1 0";
-        convertStrToArr (str);
+        System.out.println(convertStrToArr (str));
 
     }
 
-    private static void convertStrToArr (String str) {
+    private static int convertStrToArr (String str) {
         String[][] strArr = new String[4][4];
         int i = 0;
         int j = 0;
+        int result = 0;
         for (String s1: str.split("\n")){
             for (String s2:s1.split(" ")) {
                 strArr[i][j] = s2;
@@ -19,6 +20,8 @@ public class MainClass {
             i++;
             j = 0;
         }
+
+
        /* String[] words = str.split("\n ");
         //String[] strArr;
         System.out.println(words[0]);
@@ -34,12 +37,13 @@ public class MainClass {
 
        for (i = 0; i < strArr.length; i++) {
             for (j = 0; j < strArr[i].length; j++) {
-                System.out.print(strArr[i][j]);
+                result = result + Integer.parseInt(strArr[i][j]);
             }
-           System.out.println();
+           //System.out.println();
         }
 
         //System.out.println(strArr[2][3]);
+        return result / 2;
 
     }
 }
