b04d91785c2c348bc5d308b4fb09bee25d5a59ad
Shepico <Shepico@gmail.com>
2018-07-28 15:40:10 +0400

    Урок 2. Создан массив из строки

---
+++ src/lesson2/MainClass.java
@@ -0,0 +1,45 @@
+package lesson2;
+
+public class MainClass {
+    public static void main (String[] args) {
+        String str = "1 3 1 2\n2 3 2 2\n5 6 7 1\n3 3 1 0";
+        convertStrToArr (str);
+
+    }
+
+    private static void convertStrToArr (String str) {
+        String[][] strArr = new String[4][4];
+        int i = 0;
+        int j = 0;
+        for (String s1: str.split("\n")){
+            for (String s2:s1.split(" ")) {
+                strArr[i][j] = s2;
+                j++;
+            }
+            i++;
+            j = 0;
+        }
+       /* String[] words = str.split("\n ");
+        //String[] strArr;
+        System.out.println(words[0]);
+       /* int j = 0;
+        for (String s:words) {
+            System.out.println(s);
+            //strArr[j] = s.split(" ");
+            String[] w = s.split(" ");
+            System.out.println(w);
+           // System.out.println("s - " + s.split(" "));
+            j++;
+        }*/
+
+       for (i = 0; i < strArr.length; i++) {
+            for (j = 0; j < strArr[i].length; j++) {
+                System.out.print(strArr[i][j]);
+            }
+           System.out.println();
+        }
+
+        //System.out.println(strArr[2][3]);
+
+    }
+}
